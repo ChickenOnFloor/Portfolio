6e77156e0c2ea557ba522fcc187cb437aa4c0d47
Muhammad Jawwad Siddique <115373612+ChickenOnFloor@users.noreply.github.com>
2025-07-26 14:44:08 +0500

    Update constants.js
    Update Tech List

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,10 +1,13 @@
 export const projectsData = {
-    express: 'techIcons/Express.png',
     react: 'techIcons/React.png',
+    next: 'techIcons/Next.png',
     tailwind: 'techIcons/Tailwind.png',
     typescript: 'techIcons/Typescript.png',
     javascript: 'techIcons/Javascript.png',
     html: 'techIcons/Html.png',
     css: 'techIcons/Css.png',
+    express: 'techIcons/Express.png',
+    node: 'techIcons/Node.png',
+    mongo: 'techIcons/Mongo.png',
     git: 'techIcons/Git.png',
 }
